062c8b685dae951d96aa6ab5e979480b42120b89
William Hazell <whazell@mail.uoguelph.ca>
2016-11-18 14:38:10 -0500

    Fixed sin_test by normalizing data

---
+++ tests/sin-gen.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <linux/random.h>
+#include <time.h>
+#include <math.h>
+
+
+int main() {
+	
+	srand(time(NULL));
+	// Random numbers from [-10, 10]
+	double interval = 2;
+	double div = RAND_MAX / (interval * 2);
+	
+	for (int i = 0; i < 150; i++) {
+		
+		double rnum = -interval + ((double)rand() / div);
+		double sin_val = sin(rnum);
+		double transformed = 0.5 * (sin_val + 1);
+
+		printf("%lf;%lf\n", rnum, transformed);
+	}
+}
--- tests/sin_test.c
+++ tests/sin_test.c
@@ -25,25 +25,33 @@ double sigmoid_prime (double val) {
 
 
 int main() {
-	int topology[3] = { 1, 3, 1 };
+	int topology[4] = { 1, 5, 5, 1 };
 	net* nn = malloc(sizeof(net));
 	data_set* data;
 	FILE* fh = fopen("tests/sin.csv", "r");
 
 	data = parse_from_csv(fh);
-	init_net(nn, 3, topology, sigmoid, sigmoid_prime, 0.3);
+	init_net(nn, 4, topology, sigmoid, sigmoid_prime, 0.3);
 	train(nn, data, 5000);
 
 	printf("\n\nRESULTS:\n");
 	for (int i = 0; i < 5; i++) {
-		double div = RAND_MAX / 4;
+
+		double div = RAND_MAX / (2 * 2);
 		double nrand = -2 + ((double)rand() / div);
+		
 		printf("sin(%lf) = %lf \t\t", nrand, sin(nrand));
+	
 		matrix_t* in = malloc(sizeof(matrix_t));
 		init_matrix(in, 1, 1);
 		in->matrix[0][0] = nrand;
 		matrix_t* res = predict(nn, in);
-		printf("GOT: %lf\n", res->matrix[0][0]);
+
+		/* Transform the value back to regular range,
+		 * member the transformation was:
+		 * 0.5 * sinx + 0.5 
+		 */
+		printf("GOT: %lf\n", (2 * res->matrix[0][0]) - 1);
 	}
 	
 	fclose(fh);
