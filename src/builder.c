ecd305a0f4b7b7ffa413e910dbc4b2cd5727223e
William Hazell <whazell@mail.uoguelph.ca>
2016-11-28 14:12:40 -0500

    Removed spaghetti parser

--- src/builder.c
+++ src/builder.c
@@ -8,7 +8,7 @@ static int chomp (char* str) {
 	while (*str != '\n') {
 		if (*str == '\0')
 			return SUCCESS;
-		*str++;
+		(*str)++;
 	}	
 	*str = '\0';
 	return SUCCESS;
@@ -101,164 +101,15 @@ int load_net (net* n, FILE* fh) {
 }
 
 static int parse_net (net* n, FILE* fh) {
-	if (n == NULL || fh == NULL) return FAILURE;
-	char* line_buff = malloc(sizeof(char) * MAXLINELEN);
-	char *key, *val;
-	
-	/* Ensure the file begins with BEGIN NET */	
-	if (fgets(line_buff, MAXLINELEN, fh) == NULL) return FAILURE;
-	chomp(line_buff);
-	if (strcmp(line_buff, "BEGIN NET") != 0) return FAILURE;
 
-	/* Get the layer count */
-	if (fgets(line_buff, MAXLINELEN, fh) == NULL) return FAILURE;
-	chomp(line_buff);
-	key = strtok(line_buff, "=");
-	val = strtok(NULL, "=");
-	sscanf(val, "%d", &(n->layer_count));
-
-	/* Get the topology */
-	int* topology = malloc(sizeof(int) * n->layer_count);
-	int i = 0;
-	if (fgets(line_buff, MAXLINELEN, fh) == NULL) return FAILURE;
-	chomp(line_buff);
-
-	key = strtok(line_buff, ",");
-	while (key != NULL) {
-		sscanf(key, "%d", &(topology[i]));
-		key = strtok(NULL, ",");
-		i++;
-	}
-	
-	init_net(n, n->layer_count, topology, NULL, NULL, 0.2);
-
-	int done = 0;
-	int cur_layer = 0;
-
-	while (!done) {
-		if (fgets(line_buff, MAXLINELEN, fh) == NULL) return FAILURE;
-		chomp(line_buff);
-
-		if (strcmp(line_buff, "BEGIN LAYER") == 0) {
-			
-			if (cur_layer >= n->layer_count) {
-				fprintf(stderr, "ERROR: Too many layers defined\n");
-				return FAILURE;
-			}
-
-			if (parse_layer(&(n->layers[cur_layer]), fh) == FAILURE)
-				return FAILURE;
-
-			cur_layer++;
-			continue;
-		}
-		
-		if (strcmp(line_buff, "END NET") == 0) 
-			break;
-
-		fprintf(stderr, "Unexpected input: %s\nExpected BEGIN LAYER or END NET\n", line_buff);
-	}
-
-	free(line_buff);
-	free(topology);
-	return SUCCESS;
 }
 
 static int parse_layer (layer** l, FILE* fh) {
-	if (l == NULL || fh == NULL) return FAILURE;
-	*l = malloc(sizeof(layer));
-	int done = 0;
 
-	while (!done) {
-		char* line_buff = malloc(sizeof(char) * MAXLINELEN);
-		char *key, *val;
-		
-		if (fgets(line_buff, MAXLINELEN, fh) == NULL) return FAILURE;
-		chomp(line_buff);
-
-		if (strcmp(line_buff, "BEGIN MATRIX") == 0) {
-			(*l)->weights = malloc(sizeof(matrix_t));
-			int ret = parse_matrix(&((*l)->weights), fh);
-			if (ret == FAILURE) return FAILURE;
-			continue;
-		}
-
-		if (strcmp(line_buff, "END LAYER") == 0)
-			break;
-
-		key = strtok(line_buff, "=");
-		val = strtok(NULL, "=");
-
-		if ( strcmp(key, "type") == 0 ) {
-			if (strcmp(val, "input") == 0) (*l)->ltype = input;
-			if (strcmp(val, "hidden") == 0) (*l)->ltype = hidden;
-			if (strcmp(val, "output") == 0) (*l)->ltype = output;
-		} 
-		else if ( strcmp(key, "input-nodes") == 0) 
-			sscanf(val, "%d", &((*l)->input_nodes));
-			//(*l)->input_nodes = strtol(val, NULL, 10);		
-		else if ( strcmp(key, "output-nodes") == 0) 
-			sscanf(val, "%d", &((*l)->output_nodes));
-			//(*l)->output_nodes = strtol(val, NULL, 10);
-		else if ( strcmp(key, "bias-nodes") == 0) 
-			(*l)->bias = strtod(val, NULL);
-		
-		free(line_buff);
-	}
-	return SUCCESS;
 }
 
 static int parse_matrix (matrix_t** m, FILE* fh) {
-	if (m == NULL || fh == NULL) return FAILURE;
 
-	/* We can't use init_matrix until we know the dimensions, so for now
-	 * just keep in a 1d buffer and track the line width */
-	double* buff = malloc(sizeof(double));
-
-	*m = malloc(sizeof(matrix_t));
-	int done = 0;
-
-	/* Each Line corresponds to the i value, the 
-	 * j corresponds to each csv value */
-	int j, rows, columns;
-	j = rows = 0;
-	columns = 1;
-
-	while (!done) {
-		rows++;
-		char* line_buff = malloc(sizeof(char) * MAXLINELEN);
-		char* token;
-
-		if (fgets(line_buff, MAXLINELEN, fh) == NULL) return FAILURE;
-		chomp(line_buff);
-
-		if ( strcmp(line_buff, "END MATRIX") == 0 ) {
-			done = 1;
-		} else {
-			token = strtok(line_buff, ",");
-			while (token != NULL) {
-				j++;
-				buff = realloc(buff, sizeof(double) * rows * j);
-				buff[(4 * rows-1) + j-1] = strtod(token, NULL);
-				token = strtok(NULL, ",");
-			}
-			
-			if (columns == 1 && j > 0) columns = j;
-			else if (columns != j) return FAILURE;
-
-		}
-		free(line_buff);
-	}
-
-	init_matrix(*m, rows, columns);
-
-	for (int i = 0; i < rows; i++) {
-		for (int n = 0; n < columns; n++) {
-			(*m)->matrix[i][n] = buff[(4 * i) + n];
-		}
-	}
-	free(buff);
-	return SUCCESS;
 }
 
 int save_net (net* n, FILE* fh) {
